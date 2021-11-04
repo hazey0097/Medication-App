a1bb98aa245b2fda7f2b42e7e5d94f6d0505f7e3
sben502 <sarahxbenson@gmail.com>
2021-11-04 10:44:34 -0600

    basic nav established

--- App.js
+++ App.js
@@ -1,92 +1,49 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
-import {Image} from 'react-native' ; 
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator} from 'react-navigation-stack';
 
-export default class App extends React.Component {
-  state={
-    email:"",
-    password:""
-  }
-  render(){
-    return (
-      <View style={styles.container}>
-        <Image source={require('./logo.png')} style={{ width: 200, height: 200 }}/>
-        <View style={styles.inputView} >
-          <TextInput  
-            style={styles.inputText}
-            placeholder="Email..." 
-            placeholderTextColor="black"
-            onChangeText={text => this.setState({email:text})}/>
-        </View>
-        <View style={styles.inputView} >
-          <TextInput  
-            secureTextEntry
-            style={styles.inputText}
-            placeholder="Password..." 
-            placeholderTextColor="black"
-            onChangeText={text => this.setState({password:text})}/>
-        </View>
-        <TouchableOpacity>
-          <Text style={styles.forgot}>Forgot Password?</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.loginBtn}>
-          <Text style={styles.loginText}>LOGIN</Text>
-        </TouchableOpacity>
-        <TouchableOpacity>
-          <Text style={styles.signupText}>Signup</Text>
-        </TouchableOpacity>
+//import all the screens we are going to switch
+import HomePage from "./HomePage";
+import LoginPage from "./LoginPage";
+import SymptomsPage from "./SymptomsPage";
+import RegisterPage from "./RegisterPage";
+import MedinfoPage from "./MedinfoPage";
 
-  
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#ebf2ef',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  logo:{
-    alignItems: 'center',
-    marginBottom:40,
-    width: 20,
-    height:20
-  },
-  inputView:{
-    width:"80%",
-    backgroundColor:"#cdd1d1",
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
-  },
-  inputText:{
-    height:50,
-    color:"black"
-  },
-  forgot:{
-    color:"black",
-    fontSize:11
-  },
-  loginBtn:{
-    width:"80%",
-    backgroundColor:"#083b54",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
-    marginBottom:10
-  },
-  loginText:{
-    color:"white"
-  },
-  signupText:{
-    color:"black"
-  }
-});
+//Constant which holds all the screens like index of any book
+const App = createStackNavigator({
+        HomePage: {
+          screen: HomePage,
+          navigationOptions: {
+              header: null // Will hide header for the page - we want this
+          }
+        },
+        LoginPage: {
+          screen: LoginPage,
+          navigationOptions: {
+              header: null
+          }
+        },
+        SymptomsPage: {
+            screen: SymptomsPage,
+            navigationOptions: {
+                header: null
+            }
+        },
+        RegisterPage: {
+            screen: RegisterPage,
+            navigationOptions: {
+                header: null
+            }
+        },
+        MedinfoPage: {
+            screen: MedinfoPage,
+            navigationOptions: {
+                header: null
+            }
+        },
+    },
+    {
+      initialRouteName: 'LoginPage', //Makes the LoginPage the first main page
+    }
+);
+export default createAppContainer(App);
---
+++ RegisterPage.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {Image} from 'react-native' ;
+import HomePage from "./HomePage";
+
+export default class App extends React.Component {
+  state={
+    email:"",
+    password:""
+  }
+
+  render(){
+    const { navigate } = this.props.navigation;
+    return (
+      <View style={styles.container}>
+        <Image source={require('./logo.png')} style={{ width: 200, height: 200 }}/>
+        <View style={styles.inputView} >
+          <TextInput
+            style={styles.inputText}
+            placeholder="Email..."
+            placeholderTextColor="black"
+            onChangeText={text => this.setState({email:text})}/>
+        </View>
+        <View style={styles.inputView} >
+          <TextInput
+            secureTextEntry
+            style={styles.inputText}
+            placeholder="Password..."
+            placeholderTextColor="black"
+            onChangeText={text => this.setState({password:text})}/>
+        </View>
+        <TouchableOpacity style={styles.loginBtn} onPress={() =>navigate('HomePage')}>
+          <Text style={styles.loginText}>SIGN UP</Text>
+        </TouchableOpacity>
+        <TouchableOpacity>
+          <Text style={styles.signupText} onPress={() =>navigate('LoginPage')}>Login</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ebf2ef',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  logo:{
+    alignItems: 'center',
+    marginBottom:40,
+    width: 20,
+    height:20
+  },
+  inputView:{
+    width:"80%",
+    backgroundColor:"#cdd1d1",
+    borderRadius:25,
+    height:50,
+    marginBottom:20,
+    justifyContent:"center",
+    padding:20
+  },
+  inputText:{
+    height:50,
+    color:"black"
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#083b54",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginText:{
+    color:"white"
+  },
+  signupText:{
+    color:"black"
+  },
+});
