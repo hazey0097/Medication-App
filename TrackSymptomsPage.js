e129739f03a499f70e0f32fc680372fb13e37b92
sben502 <sarahxbenson@gmail.com>
2021-11-08 14:48:52 -0700

    track symptoms mostly done, see todo items on page

--- TrackSymptomsPage.js
+++ TrackSymptomsPage.js
@@ -1,14 +1,23 @@
 import React, {Component, useState} from 'react';
-import {StyleSheet, View, Button, Text, TextInput} from 'react-native';
+import {StyleSheet, View, Button, Text, TextInput, TouchableOpacity} from 'react-native';
 import Icon from "react-native-vector-icons/Ionicons";
-import {BACKGROUND, FOOTER_COLOR, HEADER_COLOR, NAV_ICON_COLOR, SUB_HEADER} from "./colors";
+import {BACKGROUND, BUTTON_FILLED, FOOTER_COLOR, HEADER_COLOR, NAV_ICON_COLOR, SUB_HEADER} from "./colors";
 import {FULL_SCREEN_WIDTH, HEADER_HEIGHT, NAV_HEIGHT, SUBHEADER_HEIGHT} from "./constants";
-
+import DatePicker from 'react-native-datepicker'
 
 export default class TrackSymptomsPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {chosenDate: new Date()};
+        this.setDate = this.setDate.bind(this);
+    }
+    setDate(newDate) {
+        this.setState({chosenDate: newDate});
+    }
 
     render() {
         const { navigate } = this.props.navigation;
+
         return (
             <View style={styles.container}>
                 {/*header*/}
@@ -20,14 +29,51 @@ export default class TrackSymptomsPage extends Component {
                     <Text>Track Symptoms Page</Text>
                 </View>
                 <View style={styles.container}>
-                    <Text>Add a symptom</Text>
-                    <Text>Date</Text>
-                    <Text>Symptom</Text>
+                    <Text style={styles.title }>Add a symptom</Text>
+                    <Text> </Text>
+                    <DatePicker
+                        style={{width: 200}}
+                        date={this.state.date}
+                        mode="date"
+                        placeholder="select date"
+                        format="DD-MM-YYYY"
+                        confirmBtnText="Confirm"
+                        cancelBtnText="Cancel"
+                        customStyles={{
+                            dateIcon: {
+                                position: 'absolute',
+                                left: 0,
+                                top: 4,
+                                marginLeft: 0
+                            },
+                            dateInput: {
+                                marginLeft: 36
+                            }
+                        }}
+                        onDateChange={(date) => {this.setState({date: date})}}
+                    />
+                    <Text> </Text>
                     <TextInput
                         style={styles.inputText}
                         placeholder="Enter Your Symptoms..."
+                        multiline={true}
+                        numberOfLines={4}
                         placeholderTextColor="black"
                         onChangeText={text => this.setState({symptoms:text})}/>
+                    <Text> </Text>
+                    <View style={styles.buttons}>
+                        {/*TODO: fix what happens with the cancel and save buttons*/}
+                        {/*TODO: make sure symptoms and date saves*/}
+                        {/*TODO: Fix title styling*/}
+                        <TouchableOpacity style={styles.cancelBtn}>
+                            <Text style={styles.cancelBtnText} onPress={() =>navigate('HomePage')}>Cancel</Text>
+                        </TouchableOpacity>
+                        <Text> </Text>
+                        <TouchableOpacity style={styles.saveBtn} onPress={() =>navigate('SymptomsPage')}>
+                            <Text style={styles.saveBtnText}>Save</Text>
+                        </TouchableOpacity>
+                    </View>
+
                 </View>
                 {/*footer*/}
                 <View style={styles.bottomNav}>
@@ -70,6 +116,47 @@ const styles = StyleSheet.create({
         width:FULL_SCREEN_WIDTH,
         height: SUBHEADER_HEIGHT,
         alignItems:"center",
+    },
+    inputText: {
+        borderColor: SUB_HEADER,
+        borderWidth: 1,
+        width: FULL_SCREEN_WIDTH - 50,
+        height: HEADER_HEIGHT,
+        padding: 3
+    },
+    buttons: {
+        flexDirection:"row",
+        justifyContent: 'space-evenly',
+    },
+    saveBtn:{
+        width:"20%",
+        backgroundColor:BUTTON_FILLED,
+        borderRadius:25,
+        height:50,
+        alignItems:"center",
+        justifyContent:"center",
+        marginTop:40,
+        marginBottom:10
+    },
+    saveBtnText:{
+        color:"white"
+    },
+    cancelBtn:{
+        width:"20%",
+        borderColor:BUTTON_FILLED,
+        borderWidth:1,
+        borderRadius:25,
+        height:50,
+        alignItems:"center",
+        justifyContent:"center",
+        marginTop:40,
+        marginBottom:10
+    },
+    cancelBtnText: {
+        color: BUTTON_FILLED
+    },
+    title: {
+        fontSize:28
     },
 
 });
