bcce36a25035f810cf5f286712bb33b32e972458
sben502 <sarahxbenson@gmail.com>
2021-11-16 08:45:59 -0700

    symptoms ordered by date correctly

--- constants.js
+++ constants.js
@@ -85,6 +85,14 @@ export const PRE_MED_INFO = [
 ]
 export const SYMPTOMS = [
     {
+        "Date": "Mon Nov 14 2021",
+        "Symptoms": "Sore throat and Dizziness"
+    },
+    {
+        "Date": "Mon Nov 13 2021",
+        "Symptoms": "Fatigue and Joint Pain"
+    },
+    {
         "Date": "Wed Nov 3 2021",
         "Symptoms": "Small cough"
     },
@@ -92,13 +100,5 @@ export const SYMPTOMS = [
         "Date": "Mon Nov 1 2021",
         "Symptoms": "Sore throat"
     },
-    {
-        "Date": "Mon Nov 13 2021",
-        "Symptoms": "Fatigue and Joint Pain"
-    },
-    {
-        "Date": "Mon Nov 14 2021",
-        "Symptoms": "Sore throat and Dizziness"
-    }
 ]
 export const Reminders = ['Adderall : 1 Pill at 8:00 AM. Food (REQUIRED).', 'Benzodiazepines : 2 Pills at 3:00 PM. Food (OPTIONAL)', 'Advil : take anytime if needed. Food (OPTIONAL).', 'Drink enough fluids with medications.']
